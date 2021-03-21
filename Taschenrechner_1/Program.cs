68c9cc50ef6f9e5f7f3ebb26a28e8f85d33aa8e8
kara <benjaminehauser@gmail.com>
2021-03-21 16:16:48 +0100

    Rechner auf Float umgestellt

--- Taschenrechner_1/Program.cs
+++ Taschenrechner_1/Program.cs
@@ -6,19 +6,19 @@ namespace Taschenrechner_1 {
             //Uer Story: Titel: Addieren Als Benutzer möchte ich Zahlen addieren, um mir deren Summe anzeigen zu lassen Anforderung: -Zahlen von 0 bis 9 können verwendet werden
 
             Console.Write("1. Summand eingeben: ");
-            int ersteZahl;
+            float ersteZahl;
 
-            while (!int.TryParse(Console.ReadLine(), out ersteZahl)) {
-                Console.Write("keine Zahl oder zu grosser Wert (Maximalwert: {0}). \n1. Summand eingeben: ", int.MaxValue);
+            while (!float.TryParse(Console.ReadLine(), out ersteZahl)) {
+                Console.Write("keine Zahl oder zu grosser Wert (Maximalwert: {0:F}). \n1. Summand eingeben: ", float.MaxValue);
             }
 
             Console.Write("2. Summand eingeben: ");
-            int zweiteZahl;
-            while (!int.TryParse(Console.ReadLine(), out zweiteZahl)) {
-                Console.Write("keine Zahl oder zu grosser Wert (Maximalwert: {0}). \n2. Summand eingeben: ", int.MaxValue);
+            float zweiteZahl;
+            while (!float.TryParse(Console.ReadLine(), out zweiteZahl)) {
+                Console.Write("keine Zahl oder zu grosser Wert (Maximalwert: {0:F}). \n2. Summand eingeben: ", float.MaxValue);
             }
 
-            UInt64 result = (UInt64)ersteZahl + (UInt64)zweiteZahl;
+            double result = ersteZahl + zweiteZahl;
 
             Console.WriteLine("Resultat: " + result +  "\n\nbeliebige Taste drücken zum Beenden...") ;
 
