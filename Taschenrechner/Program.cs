b18554aeabb567c6e61e1629f76cbc9a10df38cf
kara <benjaminehauser@gmail.com>
2021-04-19 19:56:25 +0200

    verschiebt warten auf Benutzereingabe auf separate Methode

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -3,25 +3,29 @@
 namespace Taschenrechner {
     class Program {
         static void Main(string[] args) {
-            do {
-                Console.Clear();
+
+            Console.Clear();
 
 
-                Console.Write("1. Summand eingeben: ");
-                string ersterSummand = Console.ReadLine();
+            Console.Write("1. Summand eingeben: ");
+            string ersterSummand = Console.ReadLine();
 
-                Console.Write("2. Summand eingeben: ");
-                string zweiterSummand = Console.ReadLine();
+            Console.Write("2. Summand eingeben: ");
+            string zweiterSummand = Console.ReadLine();
 
-                float ersteZahl = Convert.ToSingle(ersterSummand);
-                float zweiteZahl = Convert.ToSingle(zweiterSummand);
+            float ersteZahl = Convert.ToSingle(ersterSummand);
+            float zweiteZahl = Convert.ToSingle(zweiterSummand);
 
-                double result = ersteZahl + zweiteZahl;
+            double result = ersteZahl + zweiteZahl;
 
-                Console.WriteLine("Resultat: " + Convert.ToSingle(result) + "\n\nbeliebige Taste drücken zum Beenden...");
+            Console.WriteLine("Resultat: " + Convert.ToSingle(result) + "\n\nbeliebige Taste drücken zum Beenden...");
 
-                Console.WriteLine("E drücken zum beenden, beliebige Taste drücken für weitere Berechnung");
-            } while (Console.ReadKey().Key != ConsoleKey.E);
+            WarteAufBenutzerEingabe();
+        }
+
+        static void WarteAufBenutzerEingabe() {
+            Console.WriteLine("Zum Beenden bitte Return drücken.");
+            Console.ReadLine();
         }
     }
 }
