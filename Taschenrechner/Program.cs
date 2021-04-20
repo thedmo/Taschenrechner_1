cc8a4802619205b4c56e513c39c7dff20ba3c941
kara <benjaminehauser@gmail.com>
2021-04-20 10:43:09 +0200

    Fügt Kommentare zu den einzelnen Schritten ein

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -3,19 +3,25 @@
 namespace Taschenrechner {
     class Program {
         static void Main(string[] args) {
-
+            //Ausgabe leeren
             Console.Clear();
 
+            //Eingabe
             string ersterSummand = HoleEingabe("1. Summand eingeben: ");
             string zweiterSummand = HoleEingabe("2. Summand eingeben: ");
 
+            //Umwandlung zu Zahl
+            // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
             double ersteZahl = Convert.ToDouble(ersterSummand);
             double zweiteZahl = Convert.ToDouble(zweiterSummand);
 
+            //Berechnung
             double result = Addiere(ersteZahl, zweiteZahl);
 
+            //Ausgabe
             Console.WriteLine("Resultat: " + result);
 
+            //Beenden
             WarteAufBenutzerEingabe();
         }
 
