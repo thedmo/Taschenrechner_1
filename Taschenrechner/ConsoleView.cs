0829217e805360e98a7e4e77e993f430732bd01a
kara <benjaminehauser@gmail.com>
2021-05-05 16:05:17 +0200

    verschiebt Konsolenausgabe in separate Klasse

---
+++ Taschenrechner/ConsoleView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner {
+    class ConsoleView {
+
+        public string AusgabeString { get; private set; }
+
+        public ConsoleView() {
+            //Ausgabe leeren
+            Console.Clear();
+
+            //AusgabeString initialisieren
+            AusgabeString = "";
+        }
+
+        public void GebeResultatAus(string operation, double resultat) {
+
+            switch (operation) {
+                case "+":
+                    AusgabeString = "Die Summe beträgt: " + resultat;
+                    break;
+                case "-":
+                    AusgabeString = "Die Differenz beträgt: " + resultat;
+                    break;
+                case "/":
+                    AusgabeString = "Der Quotient beträgt: " + resultat;
+                    break;
+                case "*":
+                    AusgabeString = "Das Produkt beträgt: " + resultat;
+                    break;
+                default:
+                    AusgabeString = "Keine gültige Operation eingegeben..";
+                    break;
+            }
+
+            Console.WriteLine(AusgabeString);
+        }
+
+        public string HoleBenutzerEingabe(string ausgabeText) {
+            Console.Write(ausgabeText);
+            string s = Console.ReadLine();
+            return s;
+        }
+    }
+}
--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -3,13 +3,15 @@
 namespace Taschenrechner {
     class Program {
         static void Main(string[] args) {
-            //Ausgabe leeren
-            Console.Clear();
+            
+            //Klassen instantiieren
+            ConsoleView console = new ConsoleView();
+            RechnerModel model = new RechnerModel();
 
             //Eingabe
-            string ersteEingabe = HoleBenutzerEingabe("1. Wert eingeben: ");
-            string zweiteEingabe = HoleBenutzerEingabe("2. Wert eingeben: ");
-            string operation = HoleBenutzerEingabe("Operator eingeben (+, -, / oder *): ");
+            string ersteEingabe = console.HoleBenutzerEingabe("1. Wert eingeben: ");
+            string zweiteEingabe = console.HoleBenutzerEingabe("2. Wert eingeben: ");
+            string operation = console.HoleBenutzerEingabe("Operator eingeben (+, -, / oder *): ");
 
             //Umwandlung zu Zahl
             // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
@@ -17,44 +19,13 @@ namespace Taschenrechner {
             double zweiteZahl = Convert.ToDouble(zweiteEingabe);
 
             //Berechnung
-            RechnerModel model = new RechnerModel();
             model.Berechne(ersteZahl, zweiteZahl, operation);
 
             //Ausgabe
-            Ausgabe(operation, model.Resultat);
+            console.GebeResultatAus(operation, model.Resultat);
 
             //Beenden
-            HoleBenutzerEingabe("Drücke Return zum beenden...");
-        }
-
-        private static void Ausgabe(string operation, double resultat) {
-            string ausgabeString = "";
-            
-            switch (operation) {
-                case "+":
-                    ausgabeString = "Die Summe beträgt: " + resultat;
-                    break;
-                case "-":
-                    ausgabeString = "Die Differenz beträgt: " + resultat;
-                    break;
-                case "/":
-                    ausgabeString = "Der Quotient beträgt: " + resultat;
-                    break;
-                case "*":
-                    ausgabeString = "Das Produkt beträgt: " + resultat;
-                    break;
-                default:
-                    ausgabeString = "Keine gültige Operation eingegeben..";
-                    break;
-            }
-
-            Console.WriteLine(ausgabeString);
-        }
-
-        static string HoleBenutzerEingabe(string ausgabeText) {
-            Console.Write(ausgabeText);
-            string s = Console.ReadLine();
-            return s;
+            console.HoleBenutzerEingabe("Drücke Return zum beenden...");
         }
     }
 }
